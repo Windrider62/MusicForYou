fe13b33cb22ef8fdd6a90ccb8bcfee54174b8d03
windrider62 <glenny@me.com>
2018-05-15 22:25:31 +0200

    server.arg() you can send a volume parameter.

--- WebRadioPlayer/WebRadioPlayer/WebRadioPlayer.ino
+++ WebRadioPlayer/WebRadioPlayer/WebRadioPlayer.ino
@@ -44,16 +44,14 @@
          while (1) delay(10);
       }
      
-      Serial.println(F("VS1053 found"));
+      
       musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
       
       // Set volume for left, right channels. lower numbers == louder volume!
       musicPlayer.setVolume(lastvol, lastvol);
      
-      // don't use an IRQ, we'll hand-feed
-     
-
-      Serial.print("Connecting to SSID "); Serial.println(ssid);
+      
+      //connect to the network with the ssid and the password
       WiFi.begin(ssid, password);
       
       while (WiFi.status() != WL_CONNECTED) {
@@ -62,19 +60,13 @@
       }
      
       Serial.println("WiFi connected");  
-      Serial.println("IP address: ");  Serial.println(WiFi.localIP());
+      //print the ip adress
+      Serial.println(WiFi.localIP());
 
-      Serial.println("web radio path");
-      while(Serial.available()==0)
-      {
-        
-        }
-        path=Serial.readString();
-        Serial.println(path);
+      
      
       /************************* INITIALIZE STREAM */
-      Serial.print("connecting to ");  Serial.println(host);
-        Serial.println(host+path);
+     
       if (!client.connect(host, httpPort)) {
         Serial.println("Connection failed");
         return;
@@ -82,30 +74,27 @@
       
       // We now create a URI for the request
       Serial.print("Requesting URL: ");
-      Serial.println(path);
+      Serial.println(host+path);
       
       // This will send the request to the server
       client.print(String("GET ") + path + " HTTP/1.1\r\n" +
                    "Host: " + host + "\r\n" + 
                    "Connection: close\r\n\r\n");
-
-
-////testing
+                   
+       // wifi connection for hhtp requests
           WiFi.begin(ssid,password);
           while(WiFi.status() != WL_CONNECTED){
           delay(500);
           Serial.println(".");
           }
           server.begin();
-          Serial.print("Use this server to connect with hhtp://");
-          Serial.print(WiFi.localIP());
-          Serial.println("/");
-
+          
           server.on("/", OpenHomePage);
           server.on("/start", StartMusic);
           server.on("/stop", StopMusic);
           server.on("/volumeplus", VolumePlus);
           server.on("/volumemin", VolumeMin);
+          server.on("/changePath", ChangePath);
           server.onNotFound(handleNotFound);   
      
     }
@@ -115,26 +104,31 @@
     bool playingMusic=true;
 
      
-    void loop(void) {
-      
- server.handleClient(); 
-  if(playingMusic==true){
-      // wait till mp3 wants more data
-      if (musicPlayer.readyForData()) {
-        //Serial.print("ready ");
-        
-        //wants more data! check we have something available from the stream
-        if (client.available() > 0) {
-          //Serial.print("set ");
-          // yea! read up to 32 bytes
-          uint8_t bytesread = client.read(mp3buff, 32);
-          // push to mp3
-          musicPlayer.playData(mp3buff, bytesread);
+    void loop(void) 
+    {
+     // handle the http requests
+     server.handleClient(); 
+
      
-          //Serial.println("stream!");
+      if(playingMusic==true)
+      {
+        // wait till mp3 wants more data
+        if (musicPlayer.readyForData()) 
+        {
+        
+        
+            //wants more data! check we have something available from the stream
+           if (client.available() > 0) 
+            {
+              //Serial.print("set ");
+              // yea! read up to 32 bytes
+              uint8_t bytesread = client.read(mp3buff, 32);
+              // push to mp3
+              musicPlayer.playData(mp3buff, bytesread);   
+              //Serial.println("stream!");
+            }
         }
       }
-  }
     }
 
 
@@ -142,7 +136,6 @@
 void OpenHomePage(){
   server.send(200, "text/plain", "hello are yah toking to me? tok tok");
 }
-
 void StartMusic(){
   server.send(200, "text/plain", "music started");
     Serial.println("music started");
@@ -153,41 +146,71 @@ void StartMusic(){
 void StopMusic(){
   server.send(200, "text/plain", "music stopped");
   Serial.println("music stopped");
-       digitalWrite(VS1053_DCS, LOW); 
-       playingMusic=false;
+  digitalWrite(VS1053_DCS, LOW); 
+  playingMusic=false;
 }
 void VolumePlus(){
-  
-  if(lastvol-5>=0)
+  int newvol=0;
+  if(server.arg("vol")=="")
   {
-  lastvol-=5;
+    server.send(400, "text/plain", "erro: 400 no parameter was send");
   }
-  else
+  else{
+    newvol=server.arg("vol").toInt();
+    server.send(200, "text/plain", server.arg("vol"));
+  }
+  
+  if(lastvol-newvol>=0)
   {
-   lastvol=0;
+  lastvol-=newvol;
+  }
+  else{
+    lastvol=0;
   }
   server.send(200, "text/plain",  "volume="+String(lastvol));
-  Serial.println(lastvol);
   musicPlayer.setVolume(lastvol, lastvol);
   
 }
 void VolumeMin(){
+
+  int newvol=0;
+  if(server.arg("vol")=="")
+  {
+    server.send(400, "text/plain", "error: 400 no parameter was send");
+  }
+  else{
+    newvol=server.arg("vol").toInt();
+    server.send(200, "text/plain", server.arg("vol"));
+  }
   
-  if(lastvol+5<=150){
-  lastvol+=5;
+  if(lastvol+newvol<=150){
+  lastvol+=newvol;
   }
   else
   {
     lastvol=150;
   }
   server.send(200, "text/plain","volume="+String(lastvol));
-  Serial.println(lastvol);
   musicPlayer.setVolume(lastvol, lastvol);
 }
-
 void handleNotFound(){
   server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
 }
-
+void ChangePath(){
+  
+  if(server.arg("path")==""){
+    server.send(400, "text/plain", "error: 400 no parameter was send");
+  }
+  else{
+    path="/"+server.arg("path");
+    server.send(200, "text/plain", server.arg("path"));
+    
+          client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" + 
+                   "Connection: close\r\n\r\n");
+                   
+    Serial.print(host+path);          
+  }
+}
 
     
