4318ca08a935b904ba129b0c6e1139e4e9f5f32f
windrider62 <glenny@me.com>
2018-05-16 14:33:55 +0200

    swapping radio stations work

--- JavaWebRequests/src/com/company/Main.java
+++ JavaWebRequests/src/com/company/Main.java
@@ -6,12 +6,27 @@ public class Main {
     static HttpControls httpControls= new HttpControls();
 
     public static void main(String[] args) throws Exception{
-        System.out.println( StartStopMusic("http://172.20.10.4/start"));
+        System.out.println( ChangeRadioStation("http://172.20.10.4", "icecast.omroep.nl", "funx-slowjamz-sb-aac"));
     }
-    public  static String StartStopMusic(String Requesturl)throws Exception{
-        return  HttpControls.HttpGetMethod(Requesturl);
+    public  static String StartMusic(String nodeIpAdress)throws Exception{
+        String url=nodeIpAdress+"/start";
+        return  HttpControls.HttpGetMethod(url);
     }
-    public   static String ControlTheMusic(String Requesturl)throws Exception{
-        return  HttpControls.HttpGetMethod(Requesturl);
+    public  static String StopMusic(String nodeIpAdress)throws Exception{
+        String url=nodeIpAdress+"/stop";
+        return  HttpControls.HttpGetMethod(url);
+    }
+    public   static String MusicVolumePlus(String nodeIpAdress, int volume)throws Exception{
+        String url=nodeIpAdress+"/volumeplus?vol="+volume;
+        return  HttpControls.HttpGetMethod(url);
+    }
+    public   static String MusicVolumeMin(String nodeIpAdress, int volume)throws Exception{
+        String url=nodeIpAdress+"/volumemin?vol="+volume;
+        return  HttpControls.HttpGetMethod(url);
+    }
+    public static String ChangeRadioStation(String nodeIpAdress, String host, String path)throws Exception{
+        String url=nodeIpAdress+String.format("/changepath?host=%s&path=%s", host,path);
+        System.out.println(url);
+        return HttpControls.HttpGetMethod(url);
     }
 }
--- WebRadioPlayer/WebRadioPlayer/WebRadioPlayer.ino
+++ WebRadioPlayer/WebRadioPlayer/WebRadioPlayer.ino
@@ -1,114 +1,79 @@
-
+    //import library's
     #include <SPI.h>
     #include <Adafruit_VS1053.h>
     #include <ESP8266WiFi.h>
     #include <ESP8266WebServer.h> 
      
-    char* ssid     = "{unknown}";
-    const char* password = "jkw829kw91&";
-     
-
-     String host = "icecast.omroep.nl";
-     String path = "/3fm-bb-mp3";
-     int httpPort = 80;
+ 
      
     // These are the pins used
     #define VS1053_RESET   D3     // VS1053 reset pin (not used!)
     #define VS1053_CS      D8     // VS1053 chip select pin (output)
     #define VS1053_DCS     D1     // VS1053 Data/command select pin (output)
     #define VS1053_DREQ     D2     // VS1053 Data request, ideally an Interrupt pin
-
-    
-
-     
-    int lastvol = 90;
-     
     Adafruit_VS1053 musicPlayer =  Adafruit_VS1053(VS1053_RESET, VS1053_CS, VS1053_DCS, VS1053_DREQ);
-     
-    // Use WiFiClient class to create HTTP/TCP connection
-    WiFiClient client;
-    ESP8266WebServer server;
-    
-    void setup() {
-      Serial.begin(115200);
-    
-     
 
-
-     
-
-     
-      /************************* INITIALIZE MP3 WING */
-      if (! musicPlayer.begin()) { // initialise the music player
-         Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-         while (1) delay(10);
-      }
-     
-      
-      musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
-      
-      // Set volume for left, right channels. lower numbers == louder volume!
-      musicPlayer.setVolume(lastvol, lastvol);
-     
-      
-      //connect to the network with the ssid and the password
-      WiFi.begin(ssid, password);
-      
-      while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-      }
-     
-      Serial.println("WiFi connected");  
-      //print the ip adress
-      Serial.println(WiFi.localIP());
-
-      
-     
-      /************************* INITIALIZE STREAM */
-     
-      if (!client.connect(host, httpPort)) {
-        Serial.println("Connection failed");
-        return;
-      }
-      
-      // We now create a URI for the request
-      Serial.print("Requesting URL: ");
-      Serial.println(host+path);
-      
-      // This will send the request to the server
-      client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-                   "Host: " + host + "\r\n" + 
-                   "Connection: close\r\n\r\n");
-                   
-       // wifi connection for hhtp requests
-          WiFi.begin(ssid,password);
-          while(WiFi.status() != WL_CONNECTED){
-          delay(500);
-          Serial.println(".");
-          }
-          server.begin();
-          
-          server.on("/", OpenHomePage);
-          server.on("/start", StartMusic);
-          server.on("/stop", StopMusic);
-          server.on("/volumeplus", VolumePlus);
-          server.on("/volumemin", VolumeMin);
-          server.on("/changePath", ChangePath);
-          server.onNotFound(handleNotFound);   
-     
-    }
-     
-    // our little buffer of mp3 data
-    uint8_t mp3buff[32];   // vs1053 likes 32 bytes at a time
+    //global propeties
+    char* ssid     = "{unknown}";
+    const char* password = "jkw829kw91&";
+    String host = "icecast.omroep.nl";
+    String path = "/3fm-bb-mp3";
+    int httpPort = 80;
+    int lastvol = 0;
     bool playingMusic=true;
 
-     
-    void loop(void) 
-    {
-     // handle the http requests
-     server.handleClient(); 
+    //instantiate the servers
+    WiFiClient client;
+    ESP8266WebServer server;
 
+
+    //start setup
+    void setup() {
+      Serial.begin(115200);
+
+      
+       //connect to the network with the ssid and the password
+       WiFi.begin(ssid, password);
+
+       //check if the wifi connection is made
+       while (WiFi.status() != WL_CONNECTED) {
+       delay(500);
+       Serial.print(".");
+       }
+     
+       Serial.println("WiFi connected");  
+       //print the ip adress
+       Serial.println(WiFi.localIP());
+
+
+       //muisc player setup call
+       MusicPlayerSetup();
+          
+       // wifi connection for hhtp requests
+       WiFi.begin(ssid,password);
+       while(WiFi.status() != WL_CONNECTED){
+       delay(500);
+       Serial.println(".");
+       }
+
+       //http server start
+       server.begin(); 
+       //when one of those http requests are called then activate method  
+       server.on("/", OpenHomePage);
+       server.on("/start", StartMusic);
+       server.on("/stop", StopMusic);
+       server.on("/volumeplus", VolumePlus);
+       server.on("/volumemin", VolumeMin);
+       server.on("/changepath", ChangePath);
+       server.onNotFound(handleNotFound);   
+     
+    }   
+void loop(void) {
+// handle the http requests
+     server.handleClient(); 
+     
+// our little buffer of mp3 data
+    uint8_t mp3buff[32];   // vs1053 likes 32 bytes at a time
      
       if(playingMusic==true)
       {
@@ -117,11 +82,11 @@
         {
         
         
-            //wants more data! check we have something available from the stream
+//wants more data! check we have something available from the stream
            if (client.available() > 0) 
             {
-              //Serial.print("set ");
-              // yea! read up to 32 bytes
+//Serial.print("set ");
+// yea! read up to 32 bytes
               uint8_t bytesread = client.read(mp3buff, 32);
               // push to mp3
               musicPlayer.playData(mp3buff, bytesread);   
@@ -130,9 +95,32 @@
         }
       }
     }
+void MusicPlayerSetup(){
+    if (!client.connect(host, httpPort)) {
+        Serial.println("Connection failed");
+        return;
+      }
+      
+  if (! musicPlayer.begin()) { // initialise the music player
+         Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+         while (1) delay(10);
+      }
+     
+ // Make a tone to indicate VS1053 is working     
+ //musicPlayer.sineTest(0x44, 500);    
+      
+// Set volume for left, right channels. lower numbers == louder volume!
+      musicPlayer.setVolume(lastvol, lastvol);
 
 
-    
+      Serial.print("Requesting URL: ");
+      Serial.println(host+path);
+      
+// This will send the request to the server
+      client.print(String("GET ") + path + " HTTP/1.1\r\n" +
+                   "Host: " + host + "\r\n" + 
+                   "Connection: close\r\n\r\n");
+}
 void OpenHomePage(){
   server.send(200, "text/plain", "hello are yah toking to me? tok tok");
 }
@@ -198,17 +186,14 @@ void handleNotFound(){
 }
 void ChangePath(){
   
-  if(server.arg("path")==""){
-    server.send(400, "text/plain", "error: 400 no parameter was send");
+  if(server.arg("path")=="" ||server.arg("host")==""){
+    server.send(400, "text/plain", "error: 400 missing a parameter ");
   }
   else{
+    host=server.arg("host");
     path="/"+server.arg("path");
-    server.send(200, "text/plain", server.arg("path"));
-    
-          client.print(String("GET ") + path + " HTTP/1.1\r\n" +
-                   "Host: " + host + "\r\n" + 
-                   "Connection: close\r\n\r\n");
-                   
+    MusicPlayerSetup();
+    server.send(200,"text/plain","new radio ="+host+path);               
     Serial.print(host+path);          
   }
 }
