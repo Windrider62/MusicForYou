65344944b4792ea7725cb2ca340400009c7a917c
windrider62 <glenny@me.com>
2018-05-16 18:54:37 +0200

    file reader test

--- JavaWebRequests/src/com/company/Main.java
+++ JavaWebRequests/src/com/company/Main.java
@@ -1,13 +1,24 @@
 package com.company;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import org.json.
+
+
 public class Main {
 
 
     static HttpControls httpControls= new HttpControls();
 
     public static void main(String[] args) throws Exception{
-        System.out.println( ChangeRadioStation("http://172.20.10.4", "icecast.omroep.nl", "funx-slowjamz-sb-aac"));
+        System.out.println( StartMusic("http://172.20.10.4"));
     }
+
+    public void readJsonRadioStations() throws FileNotFoundException {
+         parser = new JSONParser();
+        Object obj = parser.parse(new FileReader("RadioStationJson.json"));
+    }
+
     public  static String StartMusic(String nodeIpAdress)throws Exception{
         String url=nodeIpAdress+"/start";
         return  HttpControls.HttpGetMethod(url);
